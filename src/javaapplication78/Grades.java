977d90129ea4251b73cc6d748b11746a64014c3f
France199 <quiachonfrancis@gmail.com>
2024-08-21 15:21:14 +0800

    Product and Grade class added

---
+++ src/javaapplication78/Grades.java
@@ -0,0 +1,28 @@
+
+package javaapplication78;
+
+
+public class Grades {
+    
+    int id;
+    String name;
+    double pl, m, pf, f;
+    
+        public void addGrades(int sid, String sname, double spl, double sm, double spf, double sf) {
+            
+            this.id = sid;
+            this.name = sname;
+            this.pl = spl;
+            this.m = sm;
+            this.pf = spf;
+            this.f = sf;          
+        }
+        
+        public void viewGrades() {
+            double ave = (this.pl + this.m + this.pf + this.f) / 4;
+            String remarks = (ave > 3.0) ? "Failed" : "Passed";
+            
+            
+            System.out.println("%-10d %-10s %-10.2f %-10.2f %-10.2f %-10.2f %-10.2f %-10s\n", this.id, this.name, this.pl, this.m, this.pf, this.f);
+        }
+}
--- src/javaapplication78/netPay.java
+++ src/javaapplication78/netPay.java
@@ -7,9 +7,13 @@ public class netPay {
 //        slipDetails sp = new slipDetails();
 //        sp.getPay();
 
-          Product p = new Product();
-          p.addProduct(10011, "Soap", 20.00, 20, 30);
-          p.viewProduct();
+//          Product p = new Product();
+//          p.addProduct(10011, "Soap", 20.00, 20, 30);
+//          p.viewProduct();
+
+        Grades gr = new Grades();
+        gr.addGrades(0, "sname", 0, 0, 0, 0);
+        gr.viewGrades();
 
     }
 }
